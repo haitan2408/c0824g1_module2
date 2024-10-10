53bda602305c25a364fe8a1ab9ee34db3cb2847d
haitan2408 <haitan28102408@gmail.com>
2024-10-10 11:21:36 +0700

    Up git demo array

--- src/ss1_java_overview/Student.java
+++ src/ss1_java_overview/Student.java
@@ -1,2 +1,22 @@
-package ss1_java_overview;public class Student {
+package ss1_java_overview;
+
+import java.util.Scanner;
+
+public class Student {
+    public static void main(String[] args) {
+        int a = 2147483647;
+        int b = 1;
+        short c = 128;
+        int d = a + b;
+//        Tìm hiểu thêm lý do vì sao cộng vượt quá thì chuyển thành âm
+//        System.out.println(d);
+
+        double e = 300004398734856347856783456347856347854.5f;
+        System.out.println(e);
+
+        char f = '5';
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        System.out.println(a);
+    }
 }
---
+++ src/ss2_loop_array/DemoArray.java
@@ -0,0 +1,23 @@
+package ss2_loop_array;
+
+import java.util.Arrays;
+
+public class DemoArray {
+    public static void main(String[] args) {
+        int[] arr1 = {4,5,1,2,8,11,15,0};
+        System.out.println(Arrays.toString(arr1));
+        Arrays.sort(arr1);
+        System.out.println(Arrays.toString(arr1));
+//        for(int item: arr) {
+//            System.out.print(item + " ");
+//            item = item * 2;
+//        }
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = arr1[i] * 2;
+        }
+        System.out.println(Arrays.toString(arr1));
+
+        double a,b;
+
+    }
+}
